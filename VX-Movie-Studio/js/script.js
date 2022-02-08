db94d5768feb4610f0a3056cc362090f07396a2a
danielmuthama <daniel.muthama@student.moringaschool.com>
2022-02-08 23:58:50 +0300

    update files

---
+++ VX-Movie-Studio/js/script.js
@@ -0,0 +1,142 @@
+let price;
+let format_price;
+let tooping_price;
+let total = 0
+
+function GetMovie(name, size, format, reg, total) {
+    this.name = name;
+    this.size = size;
+    this.format = format;
+    this.reg = reg;
+    this.total = total;
+};
+$(document).ready(function() {
+    $("button.proceed").click(function(event) {
+
+        let pName = $(".name option:selected").val();
+        let pFormat = $("#size option:selected").val();
+        let pTrending = $("#format option:selected").val();
+        let preg = [];
+
+        $.each($("input[name='regs']:checked"), function() {
+            preg.push($(this).val());
+        })
+
+        switch (pFormat) {
+            case "0":
+                price = 0;
+                break;
+            case 'Movies_plan':
+                price = 1500;
+                break;
+            case "Series_plan":
+                price = 2500;
+                break;
+
+            case "Animations_plan":
+                price = 1200;
+                break;
+            case "All_Genre_plan":
+                price = 3500;
+            default:
+                console.log("error")
+                _
+        }
+
+        switch (pTrending) {
+            case "0":
+                c_price = 40;
+                break;
+            case "Movie":
+                c_price = 40;
+                break;
+            case "Serie":
+                c_price = 40;
+                break;
+            case "Animation":
+                c_price = 40;
+                break;
+            case "All_Genre":
+                c_price = 40;
+            default:
+                console.log("error");
+        }
+        let reg_value = preg.length * 100;
+        if ((pFormat == "0") && (pTrending == "0")) {
+            $("button.proceed").show();
+            $("#information").hide();
+            $("div.choice").hide();
+            alert("Please select the Movie size and format");
+        } else {
+
+            $("button.proceed").show();
+            $("#information").hide();
+            $("div.choice").slideDown(1200);
+        }
+
+
+        total = price + c_price + reg_value;
+        let checkoutTotal = 0;
+        checkoutTotal = checkoutTotal + total;
+
+        $("#Moviename").html($(".name option:selected").val());
+        $("#Moviesize").html($("#size option:selected").val());
+        $("#Movieformat").html($("#format option:selected").val());
+        $("#Moviereg").html(preg.join(", "));
+        $("#totals").html(total);
+
+        $("a#ccheckout").click(function() {
+            $("a#ccheckout").hide();
+            $("a#stream").show();
+            $(".wrapper").hide();
+            $("#Movietotal").append("Amount Paid: ksh. " + checkoutTotal).show();
+            alert("Transaction Successful!!!")
+        })
+
+        if (checkoutTotal == 1640) {
+            $("a#stream").click(function() {
+                $("a#stream").show();
+                $(".wrapper-1").show();
+                $(".wrapper-2").hide();
+                $(".wrapper-3").hide();
+                $("#your-orders").hide();
+                $("#Movietotal").hide();
+
+            })
+        } else if (checkoutTotal == 2640) {
+            $("a#stream").click(function() {
+                $("a#stream").show();
+                $(".wrapper-1").hide();
+                $(".wrapper-2").show();
+                $(".wrapper-3").hide();
+                $("#your-orders").hide();
+                $("#Movietotal").hide();
+
+            })
+        } else if (checkoutTotal == 1340) {
+            $("a#stream").click(function() {
+                $("a#stream").show();
+                $(".wrapper-1").hide();
+                $(".wrapper-2").hide();
+                $("#your-orders").hide();
+                $(".wrapper-3").show();
+                $("#Movietotal").hide();
+
+            })
+
+        } else {
+            return false;
+        }
+
+
+        $("a#refresh").click(function() {
+            window.location.reload();
+            $("a#refresh").hide();
+        })
+        $("a#pay").click(function() {
+            $("a#pay").hide();
+        })
+
+
+    })
+})
